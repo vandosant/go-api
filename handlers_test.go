c792c16fcb9f1136d7b4558bf54c0605b0221667
vandosant <sjskender@gmail.com>
2015-04-12 13:54:27 -0600

    Added test to check json is returned from TodoIndex

--- handlers_test.go
+++ handlers_test.go
@@ -4,12 +4,16 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"strconv"
+	"math/rand"
+	"time"
+	"strings"
 )
 
 func TestReturns200(t *testing.T) {
 	recorder := httptest.NewRecorder()
 
-	req, err := http.NewRequest("GET", "http://example.com/tasks", nil)
+	req, err := http.NewRequest("GET", "http://example.com/todos", nil)
 	if err != nil {
 		t.Errorf("Failed to create request.")
 	}
@@ -19,4 +23,33 @@ func TestReturns200(t *testing.T) {
 	if recorder.Code != http.StatusOK {
 		t.Errorf("Status code incorrect")
 	}
+}
+
+func TestReturnsJson(t *testing.T) {
+	recorder := httptest.NewRecorder()
+
+	req, err := http.NewRequest("GET", "http://example.com/todos", nil)
+	if err != nil {
+		t.Errorf("Failed to create request.")
+	}
+
+	rand := random(1, 100)
+	todoName := "Write more tests " + strconv.Itoa(rand)
+	RepoCreateTodo(Todo{Name: todoName})
+
+	TodoIndex(recorder, req)
+
+	expectedJson := `"name":"` + todoName + `"`
+
+	result := recorder.Body.String()
+
+	if strings.Contains(result, expectedJson) != true {
+		t.Errorf("json format incorrect: Actual %s, Expected: %s", result, expectedJson)
+	}
+}
+
+// helpers
+func random(min, max int) int {
+	rand.Seed(time.Now().Unix())
+	return rand.Intn(max - min) + min
 }
